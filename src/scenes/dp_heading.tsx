fc685be7e32bbcbd26ddfe716d03d1aea5f748e2
Daniel Skýpala <daniel@honza.info>
2025-05-19 15:29:55 +0200

    DP heading: Longer transition

--- src/scenes/dp_heading.tsx
+++ src/scenes/dp_heading.tsx
@@ -4,7 +4,7 @@ import {createRef, waitFor} from '@motion-canvas/core';
 export default makeScene2D(function* (view) {
     const dynamic_programming_heading = createRef<Txt>();
     view.add(<Txt ref={dynamic_programming_heading} fill={"white"} fontSize={128}></Txt>);
-    yield* dynamic_programming_heading().text("Dynamic programming", 1);
+    yield* dynamic_programming_heading().text("Dynamic programming", 2);
     yield* waitFor(5.0);
     yield* dynamic_programming_heading().opacity(0, 1);
 });
